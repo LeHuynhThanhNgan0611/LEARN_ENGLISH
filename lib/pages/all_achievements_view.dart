900c68844cbebf3efe2f4d802380ce2c6eeb9e79
Ngan Le <lehuynhthanhngan@gmail.com>
2026-02-17 23:09:34 +0700

    _buildAchievementsGrid

--- lib/pages/all_achievements_view.dart
+++ lib/pages/all_achievements_view.dart
@@ -24,7 +24,13 @@ class AllAchievementsView extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(24),
-        child: Column(children: [_buildSummaryHeader()]),
+        child: Column(
+          children: [
+            _buildSummaryHeader(),
+            SizedBox(height: 32),
+            _buildAchievementsGrid(),
+          ],
+        ),
       ),
     );
   }
@@ -61,11 +67,11 @@ class AllAchievementsView extends StatelessWidget {
                 text: TextSpan(
                   children: [
                     TextSpan(
-                      text: "8",
+                      text: "8 ",
                       style: TextStyle(
-                        fontSize: 20,
+                        color: Color(0xFFFF8A65),
+                        fontSize: 28,
                         fontWeight: FontWeight.bold,
-                        color: Colors.black87,
                       ),
                     ),
                     TextSpan(
@@ -136,6 +142,148 @@ class AllAchievementsView extends StatelessWidget {
         ),
         child: Icon(icon, color: Colors.white, size: 18),
       ),
+    );
+  }
+
+  Widget _buildAchievementsGrid() {
+    return GridView.count(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      crossAxisCount: 3,
+      mainAxisSpacing: 24,
+      crossAxisSpacing: 16,
+      childAspectRatio: 0.8,
+      children: [
+        _buildBadgeItem(
+          "Early Bird",
+          Icons.stars_rounded,
+          Colors.orange,
+          "Lvl. 3",
+        ),
+        _buildBadgeItem(
+          "Fast Learner",
+          Icons.rocket_launch_rounded,
+          Colors.blue,
+          "Lvl. 1",
+        ),
+        _buildBadgeItem(
+          "On Fire",
+          Icons.local_fire_department_rounded,
+          Colors.teal,
+          "Lvl. 2",
+        ),
+        _buildBadgeItem(
+          "Vocab King",
+          Icons.menu_book_rounded,
+          Colors.grey,
+          "Locked",
+          isLocked: true,
+        ),
+        _buildBadgeItem(
+          "Perfect Week",
+          Icons.calendar_month_rounded,
+          Colors.grey,
+          "Locked",
+          isLocked: true,
+        ),
+        _buildBadgeItem(
+          "Socialite",
+          Icons.groups_rounded,
+          Colors.grey,
+          "Locked",
+          isLocked: true,
+        ),
+        _buildBadgeItem(
+          "Grand Master",
+          Icons.emoji_events_rounded,
+          Colors.grey,
+          "Locked",
+          isLocked: true,
+        ),
+        _buildBadgeItem(
+          "Native Echo",
+          Icons.record_voice_over_rounded,
+          Colors.grey,
+          "Locked",
+          isLocked: true,
+        ),
+        _buildBadgeItem(
+          "Mind Palace",
+          Icons.psychology_rounded,
+          Colors.grey,
+          "Locked",
+          isLocked: true,
+        ),
+        _buildBadgeItem(
+          "Top Ranker",
+          Icons.workspace_premium_rounded,
+          Colors.grey,
+          "Locked",
+          isLocked: true,
+        ),
+        _buildBadgeItem(
+          "The Scribe",
+          Icons.edit_note_rounded,
+          Colors.grey,
+          "Locked",
+          isLocked: true,
+        ),
+        _buildBadgeItem(
+          "Prestige",
+          Icons.diamond_rounded,
+          Colors.grey,
+          "Locked",
+          isLocked: true,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBadgeItem(
+    String title,
+    IconData icon,
+    Color color,
+    String subtitle, {
+    bool isLocked = false,
+  }) {
+    return Column(
+      children: [
+        Container(
+          width: 80,
+          height: 80,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: isLocked ? Colors.black.withOpacity(0.03) : Colors.white,
+            border: Border.all(
+              color: isLocked ? Colors.transparent : color.withOpacity(0.5),
+              width: 2,
+            ),
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Icon(icon, color: isLocked ? Colors.black12 : color, size: 35),
+              if (isLocked)
+                const Icon(Icons.lock_rounded, size: 20, color: Colors.black26),
+            ],
+          ),
+        ),
+        SizedBox(height: 12),
+        Text(
+          title,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 2),
+        Text(
+          subtitle,
+          style: TextStyle(
+            color: isLocked ? Colors.black26 : Colors.black45,
+            fontSize: 11,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
     );
   }
 }
